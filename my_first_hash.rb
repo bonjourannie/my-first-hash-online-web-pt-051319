845281ce39e30c74860ab43094df11695c77c1ca
Annie Truex <artruex@gmail.com>
2019-05-26 03:21:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  {"your_hash" => "burgers"
+  {"fav_food" => "burgers", "my_name" => "Annie"
   
 end
 
